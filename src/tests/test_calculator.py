bdd398ab68b1e637a56e27ed5e5387e6bb2e6add
Dimitri Pierre-Charles <dimitripierrecharles@yahoo.fr>
2026-09-09 09:47:02 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -6,9 +6,24 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 
 from calculator import Calculator
 
+my_calculator = Calculator()
+
 def test_app():
     my_calculator = Calculator()
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
-# TODO: ajoutez les tests
+def test_addition():
+    assert my_calculator.addition(2, 3) == 5
+
+def test_substraction():
+    assert my_calculator.subtraction(5, 2) == 3
+
+def test_multiplication():
+    assert my_calculator.multiplication(2, 4) == 8
+
+def test_division():
+    assert my_calculator.division(10, 5) == 2
+
+def test_division_by_zero():
+    assert my_calculator.division(10, 0).startswith("Erreur")
